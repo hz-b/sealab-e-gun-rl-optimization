73a5b8627c2a816a6477eaa181d8aed527d7510b
David Meier <meier.david@uni-kassel.de>
2025-06-07 18:16:43 +0200

    Add comparison metric, refactoring, use rmse as evaluation metric

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 from critic import Critic
 import random
 import wandb
+from model_helpers import ScaledSigmoid, create_sequential
 
 class RandomIterableDataset(IterableDataset):
     def __init__(self, num_samples, input_dim, seed, device, stddev=.2, fixed_seed=False):
@@ -24,7 +25,6 @@ class RandomIterableDataset(IterableDataset):
         torch.manual_seed(self.seed)
         if not self.fixed_seed:
             self.seed = self.seed+1
-        #print(self.seed)
         x = torch.empty((self.num_samples, self.input_dim), device=self.device)
         torch.nn.init.trunc_normal_(x, mean=0.5, std=self.stddev, a=-0.5/self.stddev, b=0.5/self.stddev)
         x.clamp_(min=0.0, max=1.0)
@@ -34,19 +34,22 @@ class RandomIterableDataset(IterableDataset):
     def __len__(self):
         return self.num_samples
 
-# LightningModule
 class RandomModel(L.LightningModule):
-    def __init__(self, input_dim=8, output_dim=4, critic_net=Critic(), neuron_factor=200):
+    def __init__(self, input_dim=8, output_dim=4, critic_net=Critic(), neuron_factor=200, layer_size=None, learning_rate=1e-4, optimizer='adam', lr_scheduler=None):
         super().__init__()
         self.neuron_factor=neuron_factor
-        self.model = nn.Sequential(nn.Linear(input_dim, self.neuron_factor*5), nn.ReLU(),
-                         nn.Linear(self.neuron_factor*5, self.neuron_factor*2), nn.ReLU(),
-                         nn.Linear(self.neuron_factor*2, self.neuron_factor*1), nn.ReLU(),
-                         nn.Linear(self.neuron_factor*1, output_dim),
-                        )
-        self.optimizer = 'adam'
-        self.lr_scheduler = None#'plateau'
-        self.learning_rate = 1e-4
+        if layer_size is None:
+            self.model = nn.Sequential(nn.Linear(input_dim, self.neuron_factor*5), nn.ReLU(),
+                             nn.Linear(self.neuron_factor*5, self.neuron_factor*2), nn.ReLU(),
+                             nn.Linear(self.neuron_factor*2, self.neuron_factor*1), nn.ReLU(),
+                             nn.Linear(self.neuron_factor*1, output_dim),
+                            )
+        else:
+            self.model = create_sequential(input_dim, output_dim, layer_size, blow=neuron_factor, shrink_factor="lin")
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+        self.learning_rate = learning_rate
+        self.layer_size = layer_size
         self.critic_net = critic_net
         self.input_dim = input_dim
         self.output_dim = output_dim
@@ -60,16 +63,16 @@ class RandomModel(L.LightningModule):
         x = batch
         #loss = batch_integral_reward(self(x), x, self.log).mean()
         rewards_mean = self.critic_net(self(x), x)
-        self.log("x_pos_loss", rewards_mean[:,0].mean())
-        self.log("y_pos_loss", rewards_mean[:,1].mean())
-        self.log("size_loss", rewards_mean[:,2].mean())
+        self.log("x_pos_loss", torch.sqrt(rewards_mean[:,0].mean()))
+        self.log("y_pos_loss", torch.sqrt(rewards_mean[:,1].mean()))
+        self.log("size_loss", torch.sqrt(rewards_mean[:,2].mean()))
         loss = rewards_mean.mean()
-        self.log("train_loss", loss, prog_bar=True)
+        self.log("train_loss", torch.sqrt(loss), prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         rewards_mean = self.critic_net(self(batch), batch).mean()
-        self.log("val_loss", rewards_mean, prog_bar=True)
+        self.log("val_loss", torch.sqrt(rewards_mean), prog_bar=True)
         return rewards_mean
 
     def configure_optimizers(self):
@@ -99,7 +102,6 @@ class RandomModel(L.LightningModule):
 
         return optimizer
 
-# DataModule that updates seed per epoch
 class RandomDataModule(L.LightningDataModule):
     def __init__(self, num_samples, input_dim, output_dim, batch_size, seed, device, val_samples, val_seed):
         super().__init__()
@@ -112,14 +114,14 @@ class RandomDataModule(L.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size)
-# Usag
+
 if __name__ == "__main__":
     batch_size = 32
     num_samples = 100000
     seed = 42
     
     wandb_logger = WandbLogger(
-            name="ref2", project="berlinpro", save_dir='outputs', offline=False
+            name="ref5", project="berlinpro", save_dir='outputs', offline=False
         )
     
     wandb.finish()
---
+++ model_helpers.py
@@ -0,0 +1,55 @@
+import torch
+from torch import nn
+import math
+
+class ScaledSigmoid(nn.Module):
+    def __init__(self, scale_min=0.1, scale_max=0.9):
+        super().__init__()
+        self.scale_min = scale_min
+        self.scale_max = scale_max
+
+    def forward(self, x):
+        x = self.scale_min + (self.scale_max - self.scale_min) * x
+        x = torch.sigmoid(x)
+        x = (x - self.scale_min) / (self.scale_max - self.scale_min)
+        return x
+
+def create_sequential(self, input_length, output_length, layer_size, blow: int | float = 0, shrink_factor="log", activation_function=nn.ReLU(), last_activation=None, batch_norm=False, layer_norm=False):
+    layers = [input_length]
+    blow_disabled = blow == 1 or blow == 0
+    if not blow_disabled:
+        layers.append(input_length*blow)
+
+    if shrink_factor == "log":
+        add_layers = torch.logspace(math.log(layers[-1], 10), math.log(output_length,10), steps=layer_size+2-len(layers), base=10).long()
+        # make sure the first and last element is correct, even though rounding
+        if blow_disabled:
+            add_layers[0] = input_length
+        add_layers[-1] = output_length
+    elif shrink_factor == "lin":
+        add_layers = torch.linspace(layers[-1], output_length, steps=layer_size+2-len(layers)).long()
+    else:
+        shrink_factor = float(shrink_factor)
+        new_length = layer_size+1-len(layers)
+        add_layers = (torch.ones(new_length)*layers[-1] * ((torch.ones(new_length) * shrink_factor) ** torch.arange(new_length))).long()
+        layers = torch.cat((torch.tensor([input_length]), add_layers))
+        layers = torch.cat((layers, torch.tensor([output_length])))
+
+    if not blow_disabled:
+        layers = torch.tensor([layers[0]])
+        layers = torch.cat((layers, add_layers))
+    else:
+       layers = add_layers
+
+    nn_layers = []
+    for i in range(len(layers)-1):
+        nn_layers.append(nn.Linear(int(layers[i].item()), int(layers[i+1].item())))
+        if not i == len(layers)-2:
+            if layer_norm:
+                nn_layers.append(nn.LayerNorm(int(layers[i+1].item())))
+            nn_layers.append(activation_function)
+            if batch_norm:
+                nn_layers.append(nn.BatchNorm1d(int(layers[i+1].item())))
+        if i == len(layers)-2 and last_activation is not None:
+            nn_layers.append(last_activation)
+    return nn.Sequential(*nn_layers)
