dffb0ee95f8bc8a236e91447f3b8e2ff89e265d0
David Meier <meier.david@uni-kassel.de>
2025-07-19 02:38:21 +0200

    Add data generation script

---
+++ data_generation/merge_datasets.py
@@ -0,0 +1,35 @@
+import h5py
+import numpy as np
+import glob
+import re
+import os
+from tqdm import tqdm
+
+# Use glob to find all matching files
+file_pattern = '../datasets/raw/bbp_ds_10m_*.h5'
+files = glob.glob(file_pattern)
+
+# Sort files numerically based on the number in the filename
+def extract_number(filename):
+    match = re.search(r'bbp_ds_10m_(\d+)\.h5', os.path.basename(filename))
+    return int(match.group(1)) if match else -1
+
+files.sort(key=extract_number)
+
+X_list = []
+Y_list = []
+
+for filename in tqdm(files):
+    with h5py.File(filename, 'r') as f:
+        X_list.append(f['X'][:])
+        Y_list.append(f['Y'][:])
+
+# Merge along axis 0
+X_merged = np.concatenate(X_list, axis=0)
+Y_merged = np.concatenate(Y_list, axis=0)
+
+# Write merged data
+output_file = '../datasets/bbp_ds_10m_merged.h5'
+with h5py.File(output_file, 'w') as f_out:
+    f_out.create_dataset('X', data=X_merged, compression='gzip')
+    f_out.create_dataset('Y', data=Y_merged, compression='gzip')
