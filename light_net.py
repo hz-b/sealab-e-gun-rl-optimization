823fc4118e23efd88108eff1ce0fa326b2936888
David Meier <meier.david@uni-kassel.de>
2025-07-23 16:16:04 +0200

    Add new version of surrogate, add validity classifier

---
+++ light_net.py
@@ -0,0 +1,281 @@
+import math
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+import h5py
+import os
+import sys
+import wandb
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from lightning.pytorch.loggers import WandbLogger
+from argparse import ArgumentParser
+import seaborn as sns
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'data_generation')))
+from simulation import sim_Y_labels
+import matplotlib.pyplot as plt
+
+#sns.set(style="darkgrid")
+
+class MinMaxDataset(Dataset):
+    def __init__(self, x, y):
+
+        # Compute min, max, and range for features
+        self.min = torch.min(x, dim=0).values
+        self.max = torch.max(x, dim=0).values
+        self.z = self.max - self.min
+
+        # Compute min, max, and range for targets
+        self.minY = torch.min(y, dim=0).values
+        self.maxY = torch.max(y, dim=0).values
+        self.zY = self.maxY - self.minY
+
+        # Apply normalization
+        self.x_norm = self.z_score(x)
+        self.y_norm = self.z_score_y(y)
+
+    def z_score(self, x):
+        return (x - self.min) / self.z
+
+    def un_z_score(self, x):
+        return x * self.z + self.min
+
+    def z_score_y(self, y):
+        return (y - self.minY) / self.zY
+
+    def un_z_score_y(self, y):
+        return y * self.zY + self.minY
+
+    def __len__(self):
+        return len(self.x_norm)
+
+    def __getitem__(self, idx):
+        return self.x_norm[idx], self.y_norm[idx]
+
+class ZScoreDataset(Dataset):
+    def __init__(self, x, y):
+        self.mean = np.mean(x, axis=1)
+        self.std = np.std(x, axis=1)
+        self.yMean = np.mean(y, axis=1)
+        self.yStd = np.std(y, axis=1)
+        
+    def z_score(self, x):
+        return (x - self.mean) / self.std
+
+    def un_z_score(self, x):
+        return x * self.std + self.mean
+
+    def z_score_y(self, y):
+        return (y - self.yMean) / self.yStd
+
+    def un_z_score_y(self, y):
+        return y * self.yStd + self.yMean
+
+class H5Dataset(MinMaxDataset):
+    def __init__(self, path, omit_outliers=True):
+        self.data = h5py.File(path,'r')
+
+        self.x = self.data['X'][:]
+        self.y = self.data['Y'][:, :5]
+        
+        self.data.close()
+        
+        self.x = torch.from_numpy(self.x).float()
+        self.y = torch.from_numpy(self.y).float()
+
+        mask = (abs(self.y[:, 0]) < 30) & (abs(self.y[:, 1]) < 30) & (abs(self.y[:, 2]) < 30) & (abs(self.y[:, 3]) < 30)
+        isnan_mask = torch.isnan(self.y[:, :4]).any(dim=1)
+
+        #else:
+        #    self.y[~mask] = torch.tensor(outlier_replacement, device=self.x.device).repeat(self.y[~mask].shape[0], 1)
+        # perform min max first so we do not get nan everywhere
+        super().__init__(self.x, self.y[~isnan_mask])
+        self.x = self.z_score(self.x)
+        self.y[~isnan_mask] = self.z_score_y(self.y[~isnan_mask])
+        
+        if omit_outliers:
+            self.x = self.x[~isnan_mask]
+            self.y = self.y[~isnan_mask]
+        
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+
+class BerlinPro2(pl.LightningModule):
+    def __init__(self, hparams):
+        super(BerlinPro2, self).__init__()
+        self.save_hyperparameters(hparams)
+        #self.hparams = hparams
+        os.makedirs(os.path.join(self.hparams.output_dir, self.hparams.name), exist_ok=True)
+        self.net = self.create_sequential(14, 5, self.hparams.layer_size, blow=self.hparams.blow, shrink_factor=self.hparams.shrink_factor)
+        self.val_x = []
+        self.val_y = []
+        self.val_y_hat = []
+        
+    def prepare_data(self):
+        self.dataset = H5Dataset(os.path.join(self.hparams.data_root,'bbp_ds_10m_merged.h5'))
+
+        train_size = int(0.6 * len(self.dataset))
+        val_size = int(0.2 * len(self.dataset))
+        test_size = len(self.dataset) - (train_size + val_size)
+        self.train_dataset, self.val_dataset, self.test_dataset = torch.utils.data.random_split(self.dataset, [train_size, val_size, test_size])
+        
+    def forward(self, x):
+        return self.net(x)
+    
+    def create_sequential(self, input_length, output_length, layer_size, blow=0, shrink_factor="log"):
+        layers = [input_length]
+        blow_disabled = blow == 1 or blow == 0
+        if not blow_disabled:
+            layers.append(input_length*blow)
+
+        if shrink_factor == "log":
+            add_layers = torch.logspace(math.log(layers[-1], 10), math.log(output_length,10), steps=layer_size+2-len(layers), base=10).long()
+            # make sure the last element is correct, even though rounding
+            add_layers[-1] = output_length
+        elif shrink_factor == "lin":
+            add_layers = torch.linspace(layers[-1], output_length, steps=layer_size+2-len(layers)).long()
+        else:
+            shrink_factor = float(shrink_factor)
+            new_length = layer_size+1-len(layers)
+            add_layers = (torch.ones(new_length)*layers[-1] * ((torch.ones(new_length) * shrink_factor) ** torch.arange(new_length))).long()
+            layers = torch.cat((torch.tensor([input_length]), add_layers))
+            layers = torch.cat((layers, torch.tensor([output_length])))
+    
+        if not blow_disabled:
+            layers = torch.tensor([layers[0]])
+            layers = torch.cat((layers, add_layers))
+        else:
+           layers = add_layers
+
+        nn_layers = []
+        for i in range(len(layers)-1):
+            nn_layers.append(nn.Linear(layers[i].item(), layers[i+1].item()))
+            if not i == len(layers)-2:
+                nn_layers.append(nn.ReLU())
+                nn_layers.append(nn.BatchNorm1d(layers[i+1].item()))
+        return nn.Sequential(*nn_layers)
+        
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = nn.MSELoss()(y, y_hat)
+        self.log("train_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_nb):
+        x, y = batch
+        y_hat = self.forward(x)
+        test_loss = nn.MSELoss()(y_hat, y)
+        self.log("test_loss", test_loss)
+
+    def validation_step(self, batch, batch_nb):
+        x, y = batch
+        y_hat = self.forward(x)
+        val_loss = nn.MSELoss()(y_hat, y)
+        self.log("val_loss", val_loss)
+        self.val_x.append(x)
+        self.val_y.append(y)
+        self.val_y_hat.append(y_hat)
+
+    def on_validation_epoch_end(self):
+        x = torch.cat([i for i in self.val_x])
+        y = torch.cat([i for i in self.val_y])
+        y_hat = torch.cat([i for i in self.val_y_hat])
+        
+        output = {}
+        plot_data_count = 1000
+        feature_rmses = []
+        for i in range(y.shape[1]):
+            feature_rmse = nn.MSELoss(reduction="mean")(y[:,i], y_hat[:,i]).sqrt()
+            feature_rmses.append(feature_rmse)
+            self.log("feature_rmse_"+sim_Y_labels[i].replace(" ", "_").replace("/", "\\"), feature_rmse)
+
+        y_data = self.dataset.un_z_score_y(y[:plot_data_count].cpu())
+        y_hat_data = self.dataset.un_z_score_y(y_hat[:plot_data_count].cpu())
+        for i in range(y.shape[1]):
+            mask = y_data[:,i] < 30000000
+            joint = sns.jointplot(x=y_data[:,i][mask], y=y_hat_data[:,i][mask], kind='scatter').set_axis_labels("Real", "Predicted")
+            joint.ax_joint.set_ylim(bottom=y_data[:, i][mask].min(), top=y_data[:, i][mask].max())
+            joint.ax_joint.set_title(sim_Y_labels[i])
+            joint.ax_joint.plot([y_data[:,i][mask].min(), y_data[:,i][mask].max()], [y_data[:,i][mask].min(), y_data[:,i][mask].max()], color="r")
+            joint.ax_joint.text(
+            0.5, -0.15, f"RMSE: {feature_rmses[i]:.4f}", 
+            transform=joint.ax_joint.transAxes, 
+            ha='center', va='top'
+            )
+            plt.tight_layout()
+            plt.savefig('outputs/'+self.hparams.name+'/jointplot_'+str(i+1)+'.pdf')
+            wandb.log({sim_Y_labels[i].replace(" ", "_").replace("/", "\\"): wandb.Image(joint.fig)})
+            joint.fig.clf()
+            plt.close(joint.fig)
+            errorplot = sns.jointplot(x=y_data[:,i][mask], y=y_data[:,i][mask]-y_hat_data[:,i][mask], color="g").fig
+            plt.tight_layout()
+            plt.savefig('outputs/'+self.hparams.name+'/line_plot_'+str(i+1)+'.pdf')
+            plt.close(errorplot)
+
+        self.val_x.clear()
+        self.val_y.clear()
+        self.val_y_hat.clear()
+
+    def configure_optimizers(self):
+        if self.hparams.optimizer == 'adam':
+            return [torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)]
+        elif self.hparams.optimizer == 'sgd':
+            return [torch.optim.SGD(model.parameters(), lr=self.hparams.learning_rate, momentum=0.9)]
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, shuffle=True, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers, pin_memory=self.on_gpu)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, shuffle=False, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers, pin_memory=self.on_gpu)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, shuffle=False, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers, pin_memory=self.on_gpu)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):  # pragma: no cover
+        """
+        Parameters you define here will be available to your model through self.hparams
+        :param parent_parser:
+        :param root_dir:
+        :return:
+        """
+        parser = ArgumentParser(parents=[parent_parser])
+
+        # param overwrites
+        # parser.set_defaults(gradient_clip_val=5.0)
+
+        # network params
+        parser.add_argument('name', type=str)
+        parser.add_argument('--layer_size', default=5, type=int)
+        parser.add_argument('--blow', default=143., type=float)
+        parser.add_argument('--shrink_factor', default="log", type=str)
+        parser.add_argument('--learning_rate', default=0.001, type=float)
+
+        # data
+        parser.add_argument('--data_root', default='datasets', type=str)
+        parser.add_argument('--output_dir', default='outputs', type=str)
+
+        # training params (opt)
+        parser.add_argument('--batch_size', default=2048, type=int)
+        parser.add_argument('--num_workers', default=os.cpu_count(), type=int)
+        parser.add_argument('--gpus', default=0, type=int)
+        parser.add_argument('--optimizer', default='adam', type=str)
+        return parser
+
+if __name__ == '__main__':
+    pl.trainer.seed_everything(42)
+    parser = BerlinPro2.add_model_specific_args(ArgumentParser(add_help=False))
+    model = BerlinPro2(parser.parse_args())
+    
+    print(model.net)
+    
+    logger = WandbLogger(name=model.hparams.name, project="berlinpro_surrogate", save_dir=model.hparams.output_dir)
+    
+    trainer = pl.Trainer(fast_dev_run=False, check_val_every_n_epoch=10, max_epochs=1000, num_nodes=1, logger=logger, precision=32)
+    trainer.fit(model)
+    trainer.test()
---
+++ validity_classifier.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, Dataset
+from argparse import ArgumentParser
+from pytorch_lightning.loggers import WandbLogger
+from light_net import H5Dataset  # import from your file
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+        
+class ValidityClassifier(pl.LightningModule):
+    def __init__(self, hparams):
+        super().__init__()
+        self.save_hyperparameters(hparams)
+
+        self.model = nn.Sequential(
+            nn.Linear(14, 64),
+            nn.ReLU(),
+            nn.BatchNorm1d(64),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.BatchNorm1d(64),
+            nn.Linear(64, 1),
+            nn.Sigmoid()
+        )
+
+        self.loss_fn = nn.BCELoss()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        pred = self(x)
+        y = y.unsqueeze(-1)
+        loss = self.loss_fn(pred, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def on_validation_epoch_start(self):
+        self._val_outputs = []  # reset buffer
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        pred = self(x)
+        y = y.unsqueeze(-1)
+        #print(pred.shape, y.shape)
+        loss = self.loss_fn(pred, y)
+        acc = ((pred > 0.5) == y.bool()).float().mean()
+        self.log("val_loss", loss, on_epoch=True, prog_bar=True)
+        self.log("val_acc", acc, on_epoch=True, prog_bar=True)
+
+        # Store output manually
+        self._val_outputs.append({
+            'x': x.detach().cpu(),
+            'pred': pred.detach().cpu(),
+            'label': y.detach().cpu()
+        })
+
+    def on_validation_epoch_end(self):
+        if not hasattr(self, "_val_outputs") or len(self._val_outputs) == 0:
+            return
+
+        limit = 1000
+        all_x = torch.cat([o['x'] for o in self._val_outputs], dim=0)[:limit]
+        all_preds = torch.cat([o['pred'] for o in self._val_outputs], dim=0)[:limit]
+        all_labels = torch.cat([o['label'] for o in self._val_outputs], dim=0)[:limit]
+
+        pred_labels = (all_preds > 0.5).float()
+        correct_mask = (pred_labels == all_labels).squeeze(-1)
+        incorrect_mask = ~correct_mask
+
+        test_mask = (all_labels == 1.).squeeze(-1)
+        print(all_x[test_mask][0], "results in", pred_labels[test_mask][0])
+        #print(all_x.shape)
+        
+        x = all_x.numpy()
+        print("Running t-SNE on validation features...")
+        tsne = TSNE(n_components=2, perplexity=30, random_state=42)
+        x_tsne = tsne.fit_transform(x)
+        print("xtsne", x_tsne.shape)
+        print("correctmask", correct_mask.shape)
+        print("pred_labels all_labels", pred_labels.shape, all_labels.shape)
+
+        plt.figure(figsize=(8, 6))
+        plt.scatter(x_tsne[correct_mask, 0], x_tsne[correct_mask, 1],
+                    c='green', label='Correct', alpha=0.5, s=10)
+        plt.scatter(x_tsne[incorrect_mask, 0], x_tsne[incorrect_mask, 1],
+                    c='red', label='Incorrect', alpha=0.5, s=10)
+        plt.title("t-SNE of Validation Set: Correct vs Incorrect Predictions")
+        plt.legend()
+        plt.grid(True)
+
+        fig_path = "outputs/val_tsne_correct_incorrect.png"
+        plt.savefig(fig_path)
+        plt.close()
+
+        if isinstance(self.logger, WandbLogger):
+            import wandb
+            self.logger.experiment.log({"val_tsne_scatter": wandb.Image(fig_path)})
+
+        # Clear buffer
+        self._val_outputs.clear()
+
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+    
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.hparams.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.hparams.batch_size)
+
+    def prepare_data(self):
+
+        full_dataset = H5Dataset(self.hparams.data_path, omit_outliers=False)
+        un_z_scored_y = full_dataset.un_z_score_y(full_dataset.y)
+        # Define binary label: valid = 1, invalid = 0
+        #validity = (
+        #    (torch.abs(un_z_scored_y[:,0]) < 30) &
+        #    (torch.abs(un_z_scored_y[:,1]) < 30) &
+        #    (torch.abs(un_z_scored_y[:,2]) < 30) &
+        #    (torch.abs(un_z_scored_y[:,3]) < 30)
+        #).float()
+        
+        #print(f"{validity.sum().item()} out of {full_dataset.y[:, 0].shape[0]} samples are valid.")
+        
+        isnan_mask = torch.isnan(full_dataset.y[:, :4]).any(dim=1)
+        validity = isnan_mask.float()
+        #print(f"{isnan_mask.sum().item()} out of {full_dataset.y[:, 0].shape[0]} samples are nan.")
+        #print("full_dataset", full_dataset.x[:].max())
+
+        class ValidityDataset(Dataset):
+            def __init__(self, x, labels):
+                self.x = x
+                self.labels = labels
+
+            def __len__(self):
+                return self.x.shape[0]
+
+            def __getitem__(self, idx):
+                return self.x[idx], self.labels[idx]
+
+        dataset = ValidityDataset(full_dataset.x, validity)
+        train_size = int(0.7 * len(dataset))
+        val_size = len(dataset) - train_size
+        #print("trainsize", train_size, "val_size", val_size)
+        self.train_dataset, self.val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--batch_size', type=int, default=1024)
+        parser.add_argument('--learning_rate', type=float, default=1e-3)
+        parser.add_argument('--data_path', type=str, default='datasets/bbp_ds_10m_merged.h5')
+        parser.add_argument('--max_epochs', type=int, default=50)
+        parser.add_argument('--output_dir', default='outputs', type=str)
+        parser.add_argument('--gpus', type=int, default=1)
+        return parser
+
+if __name__ == '__main__':
+    pl.seed_everything(42)
+
+    parser = ArgumentParser()
+    parser = ValidityClassifier.add_model_specific_args(parser)
+    hparams = parser.parse_args()
+
+    model = ValidityClassifier(hparams)
+    wandb_logger = WandbLogger(project="berlinpro_validity", name="validity_clf", save_dir=model.hparams.output_dir)
+
+    trainer = pl.Trainer(
+        logger=wandb_logger,
+        max_epochs=hparams.max_epochs,
+        accelerator="gpu" if hparams.gpus > 0 else "cpu",
+        devices=hparams.gpus if hparams.gpus > 0 else 1,
+        log_every_n_steps=10,
+    )
+
+    trainer.fit(model)
