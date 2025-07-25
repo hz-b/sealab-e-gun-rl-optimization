36946cbd6c325ba8670e672c8b88f3f693177763
David Meier <meier.david@uni-kassel.de>
2025-07-25 04:31:04 +0200

    Add overall test

---
+++ test_model_overall.py
@@ -0,0 +1,24 @@
+from data_generation.simulation import simulation_parallel
+import torch
+from evaluate import *
+from model import RandomIterableDataset
+from surrogate import H5Dataset
+import os
+import pickle
+
+network_outputs_list = []
+repetitions = 2
+device=torch.device('cuda')
+model, critic_net = load_model_critic_net(device)
+ds = RandomIterableDataset(repetitions, 8, 10000000, device)
+h5ds = H5Dataset(os.path.join('datasets','bbp_ds_10m_merged.h5'))
+
+for i, state in enumerate(tqdm(ds, total=repetitions)):
+    with torch.no_grad():
+        state = state.unsqueeze(0)
+        expanded_actions, expanded_states = critic_net.expand_action_states(state, model(state))
+        merged_input = torch.cat([expanded_states, expanded_actions], dim=1)
+        merged_un_z_scored_input = h5ds.un_z_score(merged_input.cpu())
+        output = simulation_parallel(merged_un_z_scored_input[:1])
+        with open('outputs/test_model_overall_'+str(i)+'.pkl', 'wb') as handle:
+            pickle.dump(output, handle)
