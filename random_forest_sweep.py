a231073c1b6b31bcbe33903d781de09c4e5cbfa7
David Meier <meier.david@uni-kassel.de>
2025-09-09 04:16:00 +0200

    Add additional sweeps

--- random_forest_sweep.py
+++ random_forest_sweep.py
@@ -134,9 +134,11 @@ if __name__ == "__main__":
 
     param_grid = {
         "max_depth": [4, 8, 12, None],
-        "min_samples_split": [2, 5, 10],
+        "min_samples_split": [2, 5, 10, 15],
         "max_features": ["sqrt", "log2", None],
-        "n_estimators": [50, 100, 200]
+        "n_estimators": [50, 100, 200],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 4],
     }
 
     results = multi_param_sweep_rf(
