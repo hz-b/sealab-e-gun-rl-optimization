326bac9498f23facaf10ba726cfe0a3841fe9d89
David Meier <meier.david@uni-kassel.de>
2025-10-26 02:35:23 +0100

    Set validity score and reward as previously set

--- critic.py
+++ critic.py
@@ -83,8 +83,8 @@ class Critic:
 
         if penalize_invalid and hasattr(self, "validity_classifier"):
             validity_scores = self.validity_classifier(self.model.normalizer.unscore_x(prepared_input))
-            validity = (validity_scores > 0.75).squeeze(-1)
-            reward_copy[~validity] += 1.
+            validity = (validity_scores > 0.5).squeeze(-1)
+            reward_copy[~validity] += 1000.
         return reward_copy
 
     def expand_action_states(self, action_batch, state_batch):
